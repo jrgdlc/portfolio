d4066c6ee6bfb49a29c929aa1a62cf17142a6cef
Jorge <jrgdlc@gmail.com>
2024-01-07 10:54:15 -0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,11 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+function redirectTo(url) {
+    // Open the URL in a new tab or window
+    window.open(url, '_blank');
+}
+
 (function($) {
 
 	var $window = $(window),
